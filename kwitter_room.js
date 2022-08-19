4e0d231f578aa06ba0b9a7b955e22e546c944642
Xixi-Byjus <107397970+Xixi-Byjus@users.noreply.github.com>
2022-08-19 18:47:21 +0930

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXZZxo0nf2dItSyTlr1XbPPE0M4qmmzQg",
+    authDomain: "project-94-85b45.firebaseapp.com",
+    databaseURL: "https://project-94-85b45-default-rtdb.firebaseio.com",
+    projectId: "project-94-85b45",
+    storageBucket: "project-94-85b45.appspot.com",
+    messagingSenderId: "32288015639",
+    appId: "1:32288015639:web:c486e43c0b8d15baa5e352"
+  };
